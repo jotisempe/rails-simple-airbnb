18d9d9a5a3874c0bc56cf56abd9fcf643bd41138
joti sempe <josefinasempe@gmail.com>
2018-06-13 19:23:32 -0300

    starting airbnb style clone

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Tremendo derpa en pleno Palermo',
+  address: 'Niceto Vega 1934',
+  description: 'Un dpto 3 amb con mucha luz para jugar en el balcon',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Linda casa en pilar',
+  address: 'Chubut 450 Pilar',
+  description: 'un jardin enorme',
+  price_per_night: 600,
+  number_of_guests: 9
+)
+
+Flat.create!(
+  name: 'lulis dpto',
+  address: 'palermo',
+  description: 'prestamos el derpa para hacer masajes',
+  price_per_night: 70,
+  number_of_guests: 2
+)
